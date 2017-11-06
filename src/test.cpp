7677eebee22d2d1107079b387a3c3d49e8f514d5
zhayunqin@foxmail.com <zhayunqin@foxmail.com>
2017-11-06 22:05:11 +0800

    test

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : zhangyq5
+// Version     :
+// Copyright   : zhangyq
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
